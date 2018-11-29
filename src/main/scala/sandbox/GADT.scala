43263fbcdf73c3017073eb3e055fc918a277b913
Andy Czerwonka <andy.czerwonka@gmail.com>
2018-11-29 15:17:31 -0700

    GADT example

--- src/main/scala/sandbox/GADT.scala
+++ src/main/scala/sandbox/GADT.scala
@@ -1,9 +1,15 @@
 package sandbox
 
-class GADT {
+object GADT {
 
   sealed trait Expr[T]
   case class IntExpr(i: Int) extends Expr[Int]
   case class BoolExpr(b: Boolean) extends Expr[Boolean]
 
+  def eval[T](e: Expr[T]): T = e match {
+    case IntExpr(i) => i
+    case BoolExpr(b) => b
+  }
+
 }
+
---
+++ src/main/scala/sandbox/Sandbox.sc
@@ -0,0 +1,7 @@
+import sandbox.GADT._
+
+val b = BoolExpr(true)
+val i = IntExpr(2)
+
+eval(b)
+eval(i)
